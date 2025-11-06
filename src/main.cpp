be22cb039aebe78e7cfa7416146671c20f62d936
SamP-S <sam.ps1@outlook.com>
2025-11-06 23:11:25 +0000

    impl stack; improve testing

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,55 @@
 // internal
 #include "array.hpp"
 #include "queue.hpp"
+#include "stack.hpp"
 
+void testArray() {
+    std::cout << "START TEST: ARRAY" << std::endl;
+    Array<int, 3> array;
+    std::cout << "array = " << array.toString() << std::endl;
+    array.fill(1);
+    std::cout << "array = " << array.toString() << std::endl;
+    array.clear();
+    std::cout << "array = " << array.toString() << std::endl;
+    array[1] = 2;
+    std::cout << "array = " << array.toString() << std::endl;
+    array[2] = 3;
+    std::cout << "array = " << array.toString() << std::endl;
+    std::cout << "END TEST: ARRAY" << std::endl << std::endl;
+}
 
-int main() {
-    Array<int, 3> arr;
-    arr.clear();
-    arr.fill(1);
-    arr[1] = 2;
-    arr[2] = 3;
-    std::cout << "array = " << arr.toString() << std::endl;
-
-    Queue<int> q(5);
-    std::cout << "queue = " << q.toString() << std::endl;
-    for (int i = 0; i < 10; i++) {
-        q.push(i);
-        std::cout << "queue = " << q.toString() << std::endl;
+void testQueue() {
+    std::cout << "START TEST: QUEUE" << std::endl;
+    Queue<int> queue(5);
+    std::cout << "queue = " << queue.toString() << std::endl;
+    for (int i = 0; i < 12; i++) {
+        queue.push(i);
+        std::cout << "queue = " << queue.toString() << std::endl;
     }
     for (int i = 0; i < 5; i++) {
-        q.pop();
-        std::cout << "queue = " << q.toString() << std::endl;
+        queue.pop();
+        std::cout << "queue = " << queue.toString() << std::endl;
     }
-    std::cout << "queue = " << q.toString() << std::endl;
+    std::cout << "END TEST: QUEUE" << std::endl << std::endl;
+}
 
+void testStack() {
+    std::cout << "START TEST: STACK" << std::endl;
+    Stack<int> stack(5);
+    std::cout << "stack = " << stack.toString() << std::endl;
+    for (int i = 0; i < 12; i++) {
+        stack.push(i);
+        std::cout << "stack = " << stack.toString() << std::endl;
+    }
+    for (int i = 0; i < 5; i++) {
+        stack.pop();
+        std::cout << "stack = " << stack.toString() << std::endl;
+    }
+    std::cout << "END TEST: STACK" << std::endl << std::endl;
+}
+
+int main() {
+    testArray();
+    testQueue();
+    testStack();
 }
