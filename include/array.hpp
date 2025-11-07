b46b3300d7d489105b53ac0eaa8f5f1a964b5185
SamP-S <sam.ps1@outlook.com>
2025-11-07 12:53:30 +0000

    impl indexed interface

--- include/array.hpp
+++ include/array.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
+// std lib
 #include <exception>
 #include <cstring>
 #include <sstream>
+// internal
+#include "i_indexed.hpp"
 
 /*
-    Data Structure: Fixed Size Array
+    Data Structure: Statically Sized Array
 */
 
 template<typename T, size_t SIZE>
-class Array {
+class Array : public IIndexed<T> {
 private: 
     T m_data[SIZE] = {};
 
@@ -21,24 +24,24 @@ public:
     ~Array() = default;
 
     // get fixed size of array
-    size_t size() {
+    size_t size() override {
         return SIZE;
     }
 
     // fill all elements with value
-    void fill(T _value) {
+    void fill(T _value) override {
         for (size_t i = 0; i < SIZE; i++) {
             m_data[i] = _value;
         }
     }
     
     // set all values to default "0"
-    void clear() {
+    void clear() override {
         this->fill(T());
     }
     
     // overload index operator
-    T& operator[](size_t _index) {
+    T& operator[](size_t _index) override {
         if (_index >= SIZE) {
             throw std::out_of_range("Array: [] out of range");
         }
@@ -46,7 +49,7 @@ public:
     }
 
     // convert array to string
-    std::string toString() {
+    std::string toString() override {
         std::stringstream ss;
         ss << "{ " << m_data[0];
         for (size_t i = 1; i < SIZE; i++) {
