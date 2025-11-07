dbf3dc5da93edad3f66a6eb2a950101d1406230a
SamP-S <sam.ps1@outlook.com>
2025-11-07 13:49:27 +0000

    resize fills new area default (0)

--- include/global.hpp
+++ include/global.hpp
@@ -4,15 +4,19 @@
 #include <exception>
 #include <cstring>
 #include <sstream>
+#include <iostream>
 
-// resize memory at pointer keeping existing data in place
+// resize memory at pointer keeping existing data in place and default new elements
 template<typename T>
 void memresize(T** _p, size_t _size, size_t _newSize) {
-    T* pNew = (T*)malloc(_newSize * sizeof(T));
+    // allocate new memory and copy
+	T* pNew = (T*)malloc(_newSize * sizeof(T));
     std::memcpy((void*)pNew, (void*)*_p, _size * sizeof(T));
     T* pTmp = *_p;
     *_p = pNew;
     free(pTmp);
+	// back fill new elements with default value
+	std::memset((void*)(*_p + _size), T(), (_newSize - _size) * sizeof(T)); 
 }
 
 // round to next highest base 2
